207e03185c4023ec6224692afbe9ee70d1d27764
AkhundzadaRahid <axundzada@gmail.com>
2020-09-30 15:16:48 +0400

    PingPong

---
+++ PingPong/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace PingPong
+{
+    class Program
+
+    {
+        static void Main()
+        {
+            Ping ping = new Ping();
+            Pong pong = new Pong();
+            Ping.Ud v = pong.Hit;
+            Pong.Ud w = ping.Hit;
+
+            ping.Display += v;
+            pong.Display += w;
+            ping.Hit();
+        }
+
+        class Pong
+        {
+            public delegate void Ud();
+            public event Ud Display;
+            public void Hit()
+            {
+                Console.WriteLine("Pong received Ping.");
+                if (Display != null)
+                    Display();
+            }
+        }
+
+        class Ping
+        {
+            public delegate void Ud();
+            public event Ud Display;
+            public void Hit()
+            {
+                Console.WriteLine("Ping received Pong.");
+                if (Display != null)
+                    Display();
+            }
+        }
+
+    }
+}
+
